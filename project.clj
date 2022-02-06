d9a389abf669d3dd66b5b810d7886dae69d620ec
Henry HOSONO <henry.hosono@gmail.com>
2022-02-06 14:39:01 +0100

    Finish stasis configuration and setup

--- project.clj
+++ project.clj
@@ -13,4 +13,5 @@
   :main ^:skip-aot hh-website.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot      :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
+  :aliases {"build-site" ["run" "-m" "hh-website.core/export"]})
--- src/hh_website/core.clj
+++ src/hh_website/core.clj
@@ -1,16 +1,61 @@
 (ns hh-website.core
-  (:require [stasis.core :as stasis]))
+  (:require
+    [clojure.string :as cstr]
+    [stasis.core :as stasis]
+    [markdown.core :as md]
+    [hiccup.page :as hiccup]))
 
-(def pages {"/index.html" "<h1>Welcome!</h1>"})
 (def target-dir "docs")
+(def source-dir "resources")
 
-(def app (stasis/serve-pages pages))
+(defn read-and-convert! [src]
+  (let [data         (stasis/slurp-directory src #".*\.md$")
+        html-paths   (map #(cstr/replace % #".md" ".html") (keys data))
+        html-content (map md/md-to-html-string (vals data))]
+    (zipmap html-paths html-content)))
+
+(defn apply-header-footer [page]
+  (hiccup/html5
+    {:lang "en"}
+    [:head
+     [:title "Static website!"]
+     [:meta {:charset "utf-8"}]
+     [:meta {:name    "viewport"
+             :content "width=device-width, initial-scale=1.0"}]
+     [:link {:type "text/css" :href "/css/style.css" :rel "stylesheet"}]
+     [:body
+      [:div {:class "header"}
+       [:div {:class "name"}
+        [:a {:href "/"} "Home page"]
+        [:div {:class "header-right"}
+         [:a {:href "/posts"} "Posts"]]]]
+      page]
+     [:footer
+      [:p "This is the footer"]]]))
+
+(defn format-pages [m]
+  (let [html-keys (keys m)
+        page-data (map apply-header-footer (vals m))]
+    (zipmap html-keys page-data)))
+
+(defn merge-website-assets! [root-dir]
+  (let [page-map (format-pages (read-and-convert! root-dir))
+        css-map  (stasis/slurp-directory root-dir #".*\.css$")]
+    (stasis/merge-page-sources {:css   css-map
+                                :pages page-map})))
+
+(def app (stasis/serve-pages (merge-website-assets! source-dir)))
 
 (defn write-cname [out]
   (spit (str out "/CNAME") "henryhosono.me"))
 
+(defn- load-export-dir []
+  (stasis/slurp-directory target-dir #"\.[^.]+$"))
+
 (defn export []
-  (stasis/empty-directory! target-dir)
-  (stasis/export-pages pages target-dir)
-  (write-cname target-dir)
-  (println "Build COMPLETE!"))
+  (let [old-dir (load-export-dir)]
+    (stasis/empty-directory! target-dir)
+    (stasis/export-pages (merge-website-assets! source-dir) target-dir)
+    (write-cname target-dir)
+    (println "Build COMPLETE!")
+    (stasis/report-differences old-dir (load-export-dir))))
