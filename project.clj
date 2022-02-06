b7c5e8fe40a28a7514dec331d5835b29f5c8c68f
Henry HOSONO <henry.hosono@gmail.com>
2022-02-06 14:01:03 +0100

    Fix ring handler configuration

--- project.clj
+++ project.clj
@@ -2,13 +2,13 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [stasis "2.5.1"]
                  [ring "1.9.5"]]
   :plugins [[lein-ring "0.12.6"]]
+  :ring {:handler hh-website.core/app}
   :main ^:skip-aot hh-website.core
   :target-path "target/%s"
-  :profiles {:ring    {:handler core/app}
-             :uberjar {:aot      :all
+  :profiles {:uberjar {:aot      :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
